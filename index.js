e28c86e66d8ec7aff3b5eb99572c4a0836705b8d
Spongman <piersh@hotmail.com>
2017-07-12 12:07:27 -0700

    use streaming stdio

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const childProcess = require('child_process');
 const fs = require('fs');
 const npmRunPath = require('npm-run-path');
 const execFile = childProcess.execFileSync;
-const exec = childProcess.exec;
+const spawn = childProcess.spawn;
 
 
 const command = process.argv[2];
@@ -12,23 +12,22 @@ const options = process.argv.slice(3).join(' ');
 
 const commandString = command + (options.length ? ' ' + options : '');
 
-exec(commandString, {
+var opts = {
+  stdio: 'inherit',
+  shell: true,
   env: npmRunPath.env()
-}, (error, stdout, stderr) => {
+};
+
+spawn(commandString, opts, (error) => {
   if (error) {
     if(fs.existsSync('./package.json')){
     	const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
     	if(pkg.scripts && pkg.scripts[command]){
     		console.log(pkg.scripts, pkg.scripts[command]);
-    		exec('yarn run '+ commandString, (err, stdout, stderr) => {
-    			console.log(stdout);
-    			return;
-    		})
+    		spawn('yarn run '+ commandString, opts);
     	}
     }
     console.error(`exec error: ${error}`);
     return;
   }
-  console.log(`stdout: ${stdout}`);
-
 });
